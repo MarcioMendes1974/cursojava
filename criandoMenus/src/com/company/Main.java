2e894c9f37915744e639e1e807696467dd0f8738
MarcioMendes1974 <mendes.marcio.a@gmail.com>
2022-03-28 21:18:33 -0300

    Aula 04 do curso de java

---
+++ criandoMenus/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner leitor=new Scanner(System.in);
+        double n1,n2,n3
+    }
+}
