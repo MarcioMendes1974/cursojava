223ccdbd0bb67ee5ac5543ae8af87783e4a1a5b5
MarcioMendes1974 <mendes.marcio.a@gmail.com>
2022-04-09 11:18:03 -0300

    modulo2 aula1.2

--- aula1.1/src/implementacoes/ExecutarAgenda.java
+++ aula1.1/src/implementacoes/ExecutarAgenda.java
@@ -1,26 +1,44 @@
 package implementacoes;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
 
 import beans.AgendaRevisao;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-
 public class ExecutarAgenda {
-    public static void main(String[] args)throws ParseException {
-        SimpleDateFormat formataData = new SimpleDateFormat("dd/mm/aaaa");
 
-        System.out.println("executar aegnda ");
+    public static void main(String[] args) throws ParseException {
+
+        
+        // configurar o padrão de datas
+        SimpleDateFormat formataData = new SimpleDateFormat("dd/MM/yyyy");
+
+
+        System.out.println("Executar Agendamento...!!!");
+
         AgendaRevisao agenda1 = new AgendaRevisao();
-        agenda1.cliente = "ana maria ";
-        agenda1.modelo = "gol ";
-        agenda1.kmVeiculo = 15000;
-        agenda1.data =formataData.parse("07/04/2022");
-    //*    agenda1.data "13:00";
 
-        System.out.println(" nome agenda "+agenda1.cliente);
-        System.out.println(" nome agenda "+agenda1.cliente);
-        System.out.println("data "+formataData.format(agenda1.data));
+        agenda1.cliente = "Ana Maria Braga";
+        agenda1.modelo = "Gol";
+        agenda1.kmVeiculo = 15000;
+        agenda1.data = formataData.parse("07/04/2022");
+        //agenda1.hora = "13:00";
+
+        agenda1.agendar();
+
+        Scanner leitor = new Scanner(System.in);
+        agenda1.data = formataData.parse(leitor.nextLine());
+
+        System.out.println(" ");
+        System.out.println("Dados do Agendamento");
+        System.out.println("Cliente: " + agenda1.cliente);
+        System.out.println("Veiculo: " + agenda1.modelo);
+        System.out.println("Data Agenda: " + formataData.format(agenda1.data));
+        System.out.println("Data Agenda no objeto: " + agenda1.data);
+
+        System.out.println("Finalizado..!!");
 
 
     }
+
 }
