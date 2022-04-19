e0ddf9f7e098934da587de4b419d3734b0fb6460
MarcioMendes1974 <mendes.marcio.a@gmail.com>
2022-04-18 22:25:31 -0300

    modulo 2 aula5

---
+++ Aula5/src/br/com/aula5/interfaces/PadraoImposto.java
@@ -0,0 +1,7 @@
+package br.com.aula5.interfaces;
+
+public interface PadraoImposto {
+      void calcularImposto(double percentual);
+        // void descontoImposto();  tem que implementar na classe
+
+}
