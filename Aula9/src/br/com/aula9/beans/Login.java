02ce30d516b1913af6be957f24faa343af520c05
MarcioMendes1974 <mendes.marcio.a@gmail.com>
2022-04-28 19:53:52 -0300

    modulo 2 aula9

---
+++ Aula9/src/br/com/aula9/beans/Login.java
@@ -0,0 +1,11 @@
+package br.com.aula9.beans;
+
+public class Login {
+    private String usuario;
+    private String senha;
+
+    public Login(String usuario, String senha) {
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+}
