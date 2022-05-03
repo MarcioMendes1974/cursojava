68d8c0a0c07c0daf69f6412355dfcbdd01ac017d
MarcioMendes1974 <mendes.marcio.a@gmail.com>
2022-05-02 22:27:07 -0300

    modulo 2 aula10

--- Aula10/src/br/com/aula10/dao/VeiculoDao.java
+++
@@ -1,5 +0,0 @@
-package br.com.aula10.dao;
-
-public class VeiculoDao {
-    public VeiculoDao() {};
-}
--- Aula10/src/br/com/aula10/implementacao/ExecutarVeiculo.java
+++
@@ -1,7 +0,0 @@
-package br.com.aula10.implementacao;
-
-public class ExecutarVeiculo {
-    public static void main(String[] args) {
-        String opcao =
-    }
-}
